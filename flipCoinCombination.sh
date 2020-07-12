258c3929f8d02929e6bf5345314bb0a90e0cd1aa
gangadevi <kakarlagangadevi@gmail.com>
2020-07-13 01:51:51 +0530

    Singlet Combination stored in a dictionary

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,9 +1,27 @@
 #!/bin/bash -x
 echo "Combination of Singlet, Doublet and Triplet"
-flipcoin=$((RANDOM%2))
-if [[ $flipcoin -eq 1 ]]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+declare -A flipCoin
+echo "enter the number of fliptimes"
+read fliptime
+tosscount=1;
+headsCount=0
+tailsCount=0
+while [[ $tosscount -le $fliptime ]]
+do
+	flipcoin=$((RANDOM%2))
+	if [[ $flipcoin -eq 1 ]]
+	then
+		flipCoin[$tosscount]="H"
+		((headsCount++))
+	else
+		flipCoin[$tosscount]="T"
+		((tailsCount++))
+	fi
+	((tosscount++))
+done
+perH=`echo $headsCount $fliptime | awk -F " " '{a=$1;b=$2;res1=(a/b*100);print res1}'`
+echo "HEAD percentage Combination "$perH
+perT=`echo $tailsCount $fliptime | awk -F " " '{a=$1;b=$2;res1=(a/b*100);print res1}'`
+echo "TAIL percentage Combination "$perT
+echo ${flipCoin[@]}
+echo ${!flipCoin[@]}
